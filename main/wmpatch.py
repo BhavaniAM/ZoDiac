54cb7616cce1d6e00390987250111ef88ecedd02
Bhavani AM <58398927+BhavaniAM@users.noreply.github.com>
2025-04-16 21:00:57 +0530

    Update wmpatch.py

--- main/wmpatch.py
+++ main/wmpatch.py
@@ -45,30 +45,61 @@ class GTWatermark():
         watermarking_mask = self._get_watermarking_mask(gt_patch)
         return gt_patch, watermarking_mask
 
+    # def inject_watermark(self, latents): 
+    #     latents_fft = torch.fft.fftshift(torch.fft.fft2(latents), dim=(-1, -2))
+    #     if self.watermarking_mask.shape[-2:] != latents_fft.shape[-2:]:
+    #         # Resize watermarking_mask and gt_patch to match latents_fft spatial dimensions
+    #         mask_resized = torch.nn.functional.interpolate(
+    #             self.watermarking_mask.float(), size=latents_fft.shape[-2:], mode='bilinear'
+    #         ).bool()
+    #         patch_resized = torch.nn.functional.interpolate(
+    #             self.gt_patch, size=latents_fft.shape[-2:], mode='bilinear'
+    #         )
+
+    #     else:
+    #         mask_resized = self.watermarking_mask
+    #         patch_resized = self.gt_patch
+        
+    #     # latents_fft[self.watermarking_mask] = self.gt_patch[self.watermarking_mask].clone()
+    #     latents_fft = latents_fft * (~mask_resized) + patch_resized * mask_resized
+        
+    #     print("Latents FFT shape:", latents_fft.shape)
+    #     print("Mask shape:", self.watermarking_mask.shape)
+    #     print("Patch shape:", self.gt_patch.shape)  
+
+    #     latents_w = torch.fft.ifft2(torch.fft.ifftshift(latents_fft, dim=(-1, -2))).real
+    #     return latents_w
+
     def inject_watermark(self, latents): 
-        latents_fft = torch.fft.fftshift(torch.fft.fft2(latents), dim=(-1, -2))
-        if self.watermarking_mask.shape[-2:] != latents_fft.shape[-2:]:
-            # Resize watermarking_mask and gt_patch to match latents_fft spatial dimensions
+        # Resize watermark mask and patch BEFORE applying FFT
+        if self.watermarking_mask.shape[-2:] != latents.shape[-2:]:
+            # Resize in real space
             mask_resized = torch.nn.functional.interpolate(
-                self.watermarking_mask.float(), size=latents_fft.shape[-2:], mode='bilinear'
+                self.watermarking_mask.float(), size=latents.shape[-2:], mode='bilinear'
             ).bool()
             patch_resized = torch.nn.functional.interpolate(
-                self.gt_patch, size=latents_fft.shape[-2:], mode='bilinear'
+                self.gt_patch, size=latents.shape[-2:], mode='bilinear'
             )
-
         else:
             mask_resized = self.watermarking_mask
             patch_resized = self.gt_patch
-        
-        # latents_fft[self.watermarking_mask] = self.gt_patch[self.watermarking_mask].clone()
+    
+        # FFT on the latents
+        latents_fft = torch.fft.fftshift(torch.fft.fft2(latents), dim=(-1, -2))
+    
+        # Apply watermark in the frequency domain
         latents_fft = latents_fft * (~mask_resized) + patch_resized * mask_resized
-
-        print("Latents FFT shape:", latents_fft.shape)
-        print("Mask shape:", self.watermarking_mask.shape)
-        print("Patch shape:", self.gt_patch.shape)  
-
+    
+        # Inverse FFT to return to image space (take real part)
         latents_w = torch.fft.ifft2(torch.fft.ifftshift(latents_fft, dim=(-1, -2))).real
+    
+        # Debug prints
+        print("Latents FFT shape:", latents_fft.shape)
+        print("Mask resized shape:", mask_resized.shape)
+        print("Patch resized shape:", patch_resized.shape)  
+    
         return latents_w
+
 
     def eval_watermark(self, latents_w):
         latents_w_fft = torch.fft.fftshift(torch.fft.fft2(latents_w), dim=(-1, -2))
